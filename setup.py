e6a6f3152d4c3f7419a07f2c91c40c217a26c38e
ShreyashM17 <shreyash.m@fyle.in>
2025-07-20 17:18:24 +0530

    LICENSE

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
-# setup.py
 from setuptools import setup
 
+with open('Readme.md', 'r') as f:
+  description = f.read()
+
 setup(
-    name="s_vcs",                      # This name must be unique on PyPI!
-    version="0.1.0",
-    packages=["s_vcs"],
-    entry_points={
-        "console_scripts": [
-            "svcs=s_vcs.main:main",
-        ]
-    },
-    author="Shreyash Mogaveera",
-    author_email="shreyashmogaveera@gmail.com",
-    license="MIT",
-    description='A simple version control system written in Python',
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url='https://github.com/ShreyashM17/Version-control',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.7',
+  name="s_vcs",                      # This name must be unique on PyPI!
+  version="0.1.0",
+  packages=["s_vcs"],
+  entry_points={
+    "console_scripts": [
+      "svcs=s_vcs.main:main",
+    ]
+  },
+  author="Shreyash Mogaveera",
+  author_email="shreyashmogaveera@gmail.com",
+  license="MIT",
+  description='A simple version control system written in Python',
+  long_description=description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/ShreyashM17/Version-control',
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.7',
 )
